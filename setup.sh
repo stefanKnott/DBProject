1958741c75fdf066bf5039a2d5b470aeca39eb00
Stefan Knott <stkn5801@colorado.edu>
2015-10-26 12:34:34 -0600

    move to redhat/centos

--- setup.sh
+++ setup.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-#THIS IS FOR UBUNTU & DEBIAN
+#THIS IS FOR REDHAT & CENTOS
 
 #install InfluxDB
-wget http://influxdb.s3.amazonaws.com/influxdb_0.9.4.2_amd64.deb
-sudo dpkg -i influxdb_0.9.4.2_amd64.deb
+wget http://influxdb.s3.amazonaws.com/influxdb-0.9.4.2-1.x86_64.rpm
+sudo yum localinstall influxdb-0.9.4.2-1.x86_64.rpm
+
 
 #Install Grafana
-sudo apt-get install -y apt-transport-https
-wget https://grafanarel.s3.amazonaws.com/builds/grafana_2.1.3_amd64.deb
-sudo apt-get install -y adduser libfontconfig
-sudo dpkg -i grafana_2.1.3_amd64.deb
-#	Add the folowing line to your /etc/apt/sources.list file:
-#	deb https://packagecloud.io/grafana/stable/debian/ wheezy main
-
-curl https://packagecloud.io/gpg.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install grafana
+sudo yum install https://grafanarel.s3.amazonaws.com/builds/grafana-2.1.3-1.x86_64.rpm
 
 #Run both
 sudo /etc/init.d/influxdb start
