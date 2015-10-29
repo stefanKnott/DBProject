19a431ea2381b7896dd8afdf4921ba1fa6461672
Stefan Knott <stkn5801@colorado.edu>
2015-10-28 23:20:53 -0600

    Get IP addr CentOS 6 vs CentOS 7

--- setup.sh
+++ setup.sh
@@ -12,3 +12,20 @@ sudo yum install https://grafanarel.s3.amazonaws.com/builds/grafana-2.1.3-1.x86_
 #Run both
 sudo /etc/init.d/influxdb start
 sudo service grafana-server start
+
+#Create sysMetrics DB
+/opt/influxdb/influx
+CREATE DATABASE sysMetrics
+exit
+
+#now compile sysMetrics
+make sysMetrics
+
+#get local IP addr for data input and front end visualization
+#get CentOS version with cat regex on /etc/*release*
+#when running ip addr:
+#	Centos 6: IP address is listed eth0
+#	Centos 7: IP address is listed enp0s3
+	
+#	run ip addr >> match on eth0/enp0s3 >> match on inet <<IP ADDR>>
+#	run ./sysMetric <<IP ADDR>>
