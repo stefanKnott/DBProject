5a3818f08ff2aa04387c52fc34dcb08e7cab7bfe
Stefan Knott <stkn5801@colorado.edu>
2015-11-10 10:55:57 -0700

    influxdb 0.9 curl syntax update

--- sysMetrics.c
+++ sysMetrics.c
@@ -1,15 +1,14 @@
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main(int argc, const char* argv[]){
     //Get metrics
     long numpages, pagesize, totalsize, totalfree, totalused, freepages, procconf, proconln;
-    
     while (1){
         //get info
+        
         freepages = sysconf( _SC_AVPHYS_PAGES);
         numpages = sysconf(_SC_PHYS_PAGES);
         pagesize = sysconf(_SC_PAGESIZE);
@@ -21,18 +20,18 @@ int main(){
         totalused = totalsize - totalfree;
         
         //curl into influxDB
-        
-        char url[] = " '192.168.100.20:8086/db/sysMetrics/series?u=root&p=root'";
-        char cmd[] = "curl -X POST -d";
-        char data[150];
-        
-        sprintf(data, "'[{\"name\":\"%s\",\"columns\":[\"Total\", \"Free\", \"Used\", \"Configured Proc\", \"Online Proc\"], \"points\":[[%lu, %lu, %lu, %lu, %lu]]}]'", "sysInfo", totalsize, totalfree, totalused, procconf, proconln);
-    
-        char curlCMD[300];
-        sprintf(curlCMD, "%s %s %s", cmd, data, url);
-        printf("CURL CMD: %s\n", curlCMD);
-    
-        system(curlCMD);
+	char cpuCMD[100] = "top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'";
+	int cpuUsage = system(cpuCMD);
+	char url[] = " 'http://localhost:8086/write?db=sysMetrics'";
+	char cmd[] = "curl -i -XPOST";
+	char data[150];
+	sprintf(data, "--data-binary 'sysInfo,host=denprdinf0017,cpuUsage=%d freeMem=%lu'", cpuUsage, totalfree);
+
+	char curlCMD[300];
+	sprintf(curlCMD, "%s %s %s", cmd, data, url);
+	printf("CURL CMD: %s\n", curlCMD);
+
+	system(curlCMD);
     }
 
     return 0;
