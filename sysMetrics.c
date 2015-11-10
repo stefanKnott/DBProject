f2dac1bd4ca81138a07067f6d06c200dcb048e83
Stefan Knott <stkn5801@colorado.edu>
2015-11-10 14:55:14 -0700

    get free mem and cpu use via bash

--- sysMetrics.c
+++ sysMetrics.c
@@ -5,34 +5,55 @@
 
 int main(int argc, const char* argv[]){
     //Get metrics
-    long numpages, pagesize, totalsize, totalfree, totalused, freepages, procconf, proconln;
-    while (1){
+ char freeCMD[] = "free -m | grep '+.*buffers' | awk '{print $4}'";
+        char cpuCMD[100] = "grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}'";
+        char url[] = " 'http://localhost:8086/write?db=sysMetrics'";
+        char cmd[] = "curl -i -XPOST";
+        int freeMem, cpuUseage;
+
+        while (1){
         //get info
-        
-        freepages = sysconf( _SC_AVPHYS_PAGES);
-        numpages = sysconf(_SC_PHYS_PAGES);
-        pagesize = sysconf(_SC_PAGESIZE);
-        procconf = sysconf(_SC_NPROCESSORS_CONF);
-        proconln = sysconf(_SC_NPROCESSORS_ONLN);
-        
-        totalsize = numpages * pagesize;
-        totalfree = pagesize * freepages;
-        totalused = totalsize - totalfree;
-        
-        //curl into influxDB
-	char cpuCMD[100] = "top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'";
-	int cpuUsage = system(cpuCMD);
-	char url[] = " 'http://localhost:8086/write?db=sysMetrics'";
-	char cmd[] = "curl -i -XPOST";
-	char data[150];
-	sprintf(data, "--data-binary 'sysInfo,host=denprdinf0017,cpuUsage=%d freeMem=%lu'", cpuUsage, totalfree);
+                FILE *in;
+                extern FILE *popen();
+                char buff[512];
 
-	char curlCMD[300];
-	sprintf(curlCMD, "%s %s %s", cmd, data, url);
-	printf("CURL CMD: %s\n", curlCMD);
+                if(!(in = popen(freeCMD, "r"))){
+                        exit(1);
+                }
 
-	system(curlCMD);
-    }
+                while(fgets(buff, sizeof(buff), in)!=NULL){
+                        freeMem = atoi(buff);
+                }
 
+
+                if(!(in = popen(cpuCMD, "r"))){
+                        exit(1);
+                }
+                while(fgets(buff, sizeof(buff), in)!=NULL){
+                        printf("CPU use: %s\n", buff);
+                        cpuUseage = atoi(buff);
+                }
+
+                pclose(in);
+
+                //CPU USEAGE
+                //curl into influxDB
+                char data[150];
+                sprintf(data, "--data-binary 'cpuUse,host=denprdinf0017,region=denver cpuUsage=%d'", cpuUseage);
+         
+                char curlCMD[300];
+
+		sprintf(curlCMD, "%s %s %s", cmd, data, url);
+                system(curlCMD);
+
+                char mdata[150];
+                sprintf(mdata, "--data-binary 'freeMem,host=denprdinf0017,region=denver freeMem=%d'", freeMem);
+
+                char memCMD[300];
+                sprintf(memCMD, "%s %s %s", cmd, mdata, url);
+                system(memCMD);
+
+                sleep(1);
+	}
     return 0;
 }
